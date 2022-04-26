6d05b150f4ed9d459dabe3ce293b849e93ad49c0
mayank <mayankgoyal115@gmail.com>
2022-04-26 21:39:20 +0530

    mayank

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,4 +1,5 @@
-let axios = require("axios")
+let axios = require("axios");
+const { get } = require("../routes/route");
 
 
 let getStates = async function (req, res) {
@@ -60,7 +61,7 @@ let getByPin = async function (req, res) {
 let getOtp = async function (req, res) {
     try {
         let blahhh = req.body
-        
+
         console.log(`body is : ${blahhh} `)
         var options = {
             method: "post",
@@ -78,8 +79,68 @@ let getOtp = async function (req, res) {
     }
 }
 
+let districtsId = async function (req, res) {
+    try {
+        let id = req.query.district_id;
+        let date = req.query.date;
+        var options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${id}&date=${date}`,
+        }
+        let result = await axios(options)
+        res.send({ status: true, data: result.data })
+    }
+    catch (error) {
+
+        console.log(error)
+        res.send({ status: false, msg: error.message })
+    }
+}
+
 
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.districtsId = districtsId
+
+
+//---> Assignment 2
+
+const getCityTemprature = async function (req, res) {
+    let arr = ["Bengaluru", "Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"]
+    let new_arr = []
+    for (i = 0; i < arr.length; i++) {
+        let obj = { city: arr[i] }
+        var options = {
+            method: "get",
+            url: `http://api.openweathermap.org/data/2.5/weather?q=${arr[i]}&appid=b5343e83a01bae805bfd29a3b9da3e2f`
+        }
+        let result = await axios(options)
+        obj.temp = result.data.main.temp
+        new_arr.push(obj)
+
+    }
+    let sorted = new_arr.sort(function (a, b) { return a.temp - b.temp })
+    res.status(200).send({ status: true, msg: sorted })
+}
+module.exports.getCityTemprature = getCityTemprature
+
+// ==============>  Assignment 3 <================
+
+const editMemes = async function (req, res) {
+    let template_id = req.query.template_id
+    let text0 = req.query.text0
+    let text1 = req.query.text1
+    let username = req.query.username
+    let password = req.query.password
+    
+    let options={
+        method:"post",
+        url:`https://api.imgflip.com/caption_image?template_id=${template_id}&text0=${text0}&text1=${text1}&username=${username}&password=${password}`,
+    }
+    let result=await axios(options)
+    res.send(result.data)
+}
+
+module.exports.editMemes=editMemes
--- src/routes/route.js
+++ src/routes/route.js
@@ -14,9 +14,13 @@ router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
 router.get("/cowin/getByPin", CowinController.getByPin)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
-
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
+router.get("/cowin/districtId",CowinController.districtsId)
+
+router.get("/getCityTemp",CowinController.getCityTemprature)
+
+router.post("/editMemes",CowinController.editMemes)
 
 
 module.exports = router;
